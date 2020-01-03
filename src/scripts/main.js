3ce8a63fcefb2f83a6595ef70f6112fd060c6468
Gytis Bordiugovas <gblogsitedev@gmail.com>
2020-01-03 00:31:36 +0000

    Co-authored-by: ZK <52412639+ZK-Urban@users.noreply.github.com>

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+require('../styles/styles.scss');
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('.burgerMenu');
+
+burgerExpand.addEventListener('click', function(e) {
+	document.body.classList.toggle('burgerMenu');
+});
