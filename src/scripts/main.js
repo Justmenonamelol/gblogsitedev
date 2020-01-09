d74d62b663b19bb4b31226a9b5f05fc09cf1ac24
Gytis Bordiugovas <gblogsitedev@gmail.com>
2020-01-09 22:34:17 +0000

    Co-authored-by: ZK <52412639+ZK-Urban@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
-require('../styles/styles.scss');
+import '../styles/styles.scss';
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.burgerMenu');
 burger.addEventListener('click', function(e) {
 	menu.classList.toggle('burgerMenu--display');
 });
+// gal @import cia ? :D gali import px restart? vel jis nori kazkokio css :D ar ikist dummie .css? :D
