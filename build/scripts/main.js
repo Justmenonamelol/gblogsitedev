682e780705ac9493e2f80bdb1eb7c48985a2e17a
Gytis Bordiugovas <gblogsitedev@gmail.com>
2020-01-09 23:16:32 +0000

    2020.01.09 v2

---
+++ build/scripts/main.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.burgerMenu');
+
+burger.addEventListener('click', function(e) {
+  e.preventDefault();
+  menu.classList.toggle('burgerMenu--display');
+});
