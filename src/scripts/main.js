e273ce92b023efa202b7ab6e30bb34218afe725c
Gytis Bordiugovas <gblogsitedev@gmail.com>
2020-01-09 22:40:04 +0000

    "lol2"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,3 @@ const menu = document.querySelector('.burgerMenu');
 burger.addEventListener('click', function(e) {
 	menu.classList.toggle('burgerMenu--display');
 });
-// gal @import cia ? :D gali import px restart? vel jis nori kazkokio css :D ar ikist dummie .css? :D
