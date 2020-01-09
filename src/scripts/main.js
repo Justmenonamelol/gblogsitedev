7ee2399643884a802cf871abe2e1d4b4335b6615
Gytis Bordiugovas <gblogsitedev@gmail.com>
2020-01-09 22:23:08 +0000

    lol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,6 @@ require('../styles/styles.scss');
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.burgerMenu');
-
 burger.addEventListener('click', function(e) {
 	menu.classList.toggle('burgerMenu--display');
 });
