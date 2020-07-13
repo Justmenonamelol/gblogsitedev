d65603b19bdc79dc731c15e60820cbd64a3e47c3
GB <gblogsitedev@gmail.com>
2020-07-13 22:29:32 +0100

    13 July 2020 1.0.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
-require("../styles/styles.scss");
+// require("../styles/styles.scss");
 
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".burgerMenu");
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.burgerMenu');
 
-burger.addEventListener("click", function(e) {
-	menu.classList.toggle("burgerMenu--display");
+burger.addEventListener('click', function (e) {
+	menu.classList.toggle('burgerMenu--display');
 });
